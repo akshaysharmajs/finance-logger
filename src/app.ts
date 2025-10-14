5956df97a5e24b983727d24326e9090cc9f15750
Akshay Sharma <42249933+akshaysharmajs@users.noreply.github.com>
2025-10-13 17:07:23 -0700

    add modules

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,19 @@
-// classes
-class Invoice {
-  client: string;
-  details: string;
-  amount: number;
-
-  constructor(c: string, d: string, a: number){
-    this.client = c;
-    this.details = d;
-    this.amount = a;
-  }
-
-  format() {
-    return `${this.client} owes £${this.amount} for ${this.details}`;
-  }
-}
+import { Invoice } from './classes/Invoice.js';
 
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 
+// invOne.client = 'yoshi';
+// invOne.amount = 50;
+
 let invoices: Invoice[] = [];
 invoices.push(invOne)
 invoices.push(invTwo);
-// invoices.push({ name: 'shaun' });
 
-console.log(invoices);
+invoices.forEach(inv => {
+  console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
+})
+
 
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
